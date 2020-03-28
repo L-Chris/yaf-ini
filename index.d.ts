4bb7c395e42311b589075c97f76753021c9e1088
Jiahui.liang <563303226@QQ.COM>
2020-03-28 13:08:42 +0800

    release: 0.1.0

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,4 @@
-declare module 'yaf-config-ini' {
-  export function parse(str: string): object;
-  export function decode(str: string): object;
-  export function stringify(obj: object, opt?: any): string;
-  export function encode(obj: object, opt?: any): string;
-}
+export function parse(str: string): object;
+export function decode(str: string): object;
+export function stringify(obj: object, opt?: any): string;
+export function encode(obj: object, opt?: any): string;
